8791ca1f9b746c8d3232570f6958da4ab7e87a39
Ludo <VirtualPuzzles@users.noreply.github.com>
2017-06-29 22:43:15 +0200

    Initial commit -- added gitignore -- added first extension -- added unit tests

---
+++ CsharpExtensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpExtension
+{
+    public static partial class Extensions
+    {
+        public static bool AreAllEqual<T>(this List<T> list) where T : IEquatable<T>
+            => list.GroupBy(o => o).Count() == 1;
+
+
+    }
+}
---
+++ CsharpExtensionsTests/ListExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using CsharpExtension;
+
+namespace CsharpExtensionsTests
+{
+    [TestFixture]
+    public class ListExtensionsTests
+    {
+        [Test]
+        public void AreAllStringsEqualTest()
+        {
+            new List<string> { "Test", "Test", "Test" }
+            .AreAllEqual()
+            .ShouldBeTrue();
+        }
+
+        [Test]
+        public void AreAllStringsEqualTest_Failes()
+        {
+            new List<string> { "Test2", "Test", "Test" }
+            .AreAllEqual()
+            .ShouldBeFalse();
+        }
+    }
+}
