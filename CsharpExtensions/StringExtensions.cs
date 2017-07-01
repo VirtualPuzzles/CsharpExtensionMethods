0eeea63c1ad727dfccf41268f9e7e725dd19cfab
VirtualPuzzles <VirtualPuzzles@users.noreply.github.com>
2017-07-01 19:15:04 +0200

    Added StringExtensions class

---
+++ CsharpExtensions/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace CsharpExtensions
+{
+    public static partial class Extensions
+    {
+        public static bool IsNullORWhiteSpace(this string stringToTest)
+            => string.IsNullOrWhiteSpace(stringToTest);
+
+        public static bool IsNullOrEmpty(this string stringToTest)
+            => string.IsNullOrEmpty(stringToTest);
+    }
+}
