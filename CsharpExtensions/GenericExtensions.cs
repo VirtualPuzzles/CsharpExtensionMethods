c03378ffb470240c59dbf5437c0a4da90da2e5c7
VirtualPuzzles <VirtualPuzzles@users.noreply.github.com>
2017-11-03 00:08:32 +0100

    Added cs file for generic extensions methods

---
+++ CsharpExtensions/GenericExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace CsharpExtensions
+{
+    public static partial class Extensions
+    {
+        public static bool IsNull<T>(this T obj) where T : class
+        => obj == null;
+    }
+}
