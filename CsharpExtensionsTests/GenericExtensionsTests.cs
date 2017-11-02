d93d38574add004273a9536d4d238b995b7c70ea
VirtualPuzzles <VirtualPuzzles@users.noreply.github.com>
2017-11-03 00:09:17 +0100

    Added unit tests

---
+++ CsharpExtensionsTests/GenericExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using CsharpExtensions.Tests.mocks;
+using NUnit.Framework;
+using Shouldly;
+
+namespace CsharpExtensions.Tests
+{
+    [TestFixture]
+    public class GenericExtensionsTests
+    {
+        [Test]
+        public void IsObjectNullTestWithNotNullObject()
+        {
+            var testObj = new TestClass();
+            testObj.IsNull().ShouldBeFalse();
+        }
+
+        [Test]
+        public void IsObjectNullTestWithNullObject()
+        {
+            TestClass testObj = null;
+            testObj.IsNull().ShouldBeTrue();
+        }
+
+        [Test]
+        public void IsObjectNullTestWithUnsetObject()
+        {
+            TestClass testObj = null;
+            testObj.IsNull().ShouldBeTrue();
+        }
+    }
+}
