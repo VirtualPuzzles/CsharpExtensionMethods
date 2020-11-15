a7732770b24352b9fdf8ee54f505beda3e1c0505
ludwigdn <11355187+ludwigdn@users.noreply.github.com>
2020-11-15 20:53:20 +0100

    Improve IEnumrabme.AreAllEqual()

--- CsharpExtensions/IEnumerableExtensions.cs
+++ CsharpExtensions/IEnumerableExtensions.cs
@@ -11,36 +11,11 @@ namespace CsharpExtensions
             if (enumerable.Count() == 1)
                 return true;
 
-            bool isFirstSet = false;
-            TSource firstItem = default;
-            bool isFirstItemNull = false;
-
-            using (var iter = enumerable.GetEnumerator())
-            {
-                while (iter.MoveNext())
-                {
-                    if (!isFirstSet)
-                    {
-                        var temp = iter.Current;
-
-                        if (!iter.MoveNext()) 
-                            return true;
-
-                        firstItem = temp;
-                        isFirstSet = true;
-                        isFirstItemNull = firstItem == null;
-                    }
-
-
-                    if (iter.Current == null && !isFirstItemNull)
-                        return false;
-
-                    if (iter.Current != null && !iter.Current.Equals(firstItem))
-                        return false;
-                }
-            }
-
-            return true;
+                var temp = enumerable.FirstOrDefault();
+                if (temp == null)
+                    return enumerable.Skip(1).All(o => o == null);
+                else
+                    return enumerable.Skip(1).All(o => o?.Equals(temp) ?? false);
         }
     }
 }
